bc5d3bcde980755858a66c9b19811129551ae79b
Mosina916 <mosinaashraf43@gmail.com>
2022-03-30 18:51:06 +0530

    Basics of c++

---
+++ Lecture1/Basics.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+int main() {
+     // cout<<70; //single line comment 
+    // cout<<'\n';
+
+    // cout<<'D';
+    // cout<<'\n';
+    // cout<<"coding blocks"; //multi  line comment
+
+    // cout<<70<<'\n'<<'D'<<'\n'<<"coding blocks";
+    // cout<<"df\nb";
+
+
+    // syntax
+
+    // datatype name;-->int ,float,char,bool,double-->primitive datatype
+    // int p;//declare
+    // // cout<<p; //garbage /random value
+    // p=10;//
+   
+   
+    // cout<<p<<endl;
+
+    // int p=5;//intilization
+
+    // float r=13.5643;
+    // // double d=2645.646536;
+
+    // int time=3;
+
+    // float si=(p*r*time)/100;
+
+    // cout<<"simple intrest is : "<<si<<endl;
+
+    // char  c='R';
+    // cout<<c<<endl;
+
+    // bool israining=false;
+
+    // cout<<israining<<endl;
+
+
+    // input console user
+    int p;
+    float r;
+    int t;
+
+
+    cin>>p>>r>>t;
+    // cin>>p;
+    // cin>>r;
+    // cin>>t;
+
+
+    float si=(p*r*t)/100;
+     cout<<"simple intrest is : "<<si<<endl;
+
+
+
+    return 0;
+   
+}
---
+++ Lecture1/largestof3numbers.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    // assume all 3 are different
+
+    // int x;
+    // int y;
+    // int z;
+    // int x,y,z;
+    // cin>>x>>y>>z;//
+
+    // if(condition){
+
+
+// }
+    // else if(condition){
+
+// }
+    // else{
+
+    // }
+
+
+    // if(z<x>y)//wrong 
+    // non zero value -->true be it -ve ya +ve 
+    // if(true){
+    //     cout<<"x is largest";
+
+    // }
+
+    bool israining=true;
+    // single if
+    // syntax
+    // if(condition){
+    //     // task
+    // }
+
+    // if(israining){
+    //     cout<<"take umbrella "<<endl;
+    // }
+
+
+    // if else
+
+    // if(condition){
+    //     // condition true
+    //     // task
+
+    // }
+    // else{
+
+    // }
+
+    // if(israining){
+    //     cout<<"take umbrella "<<endl;
+
+    // }
+    // else{
+    //     cout<<"Don't take umbrella "<<endl;
+    // }
+
+    // if else if else if else
+    // bool clouds=true;
+    // if(israining){
+    //      cout<<"take umbrella and raincoat "<<endl;
+
+    // }
+    // else if(clouds){
+    //     cout<<"take umbrella "<<endl;
+
+    // }
+    // else{
+    //     cout<<"Don't take anything"<<endl;
+    // }
+
+    // if can exist alone but else if and else doesn't exist without if
+    // if(israining){
+    //      cout<<"take umbrella and raincoat "<<endl;
+
+    // }
+    //  if(clouds){
+    //       cout<<"take umbrella "<<endl;
+
+    // }
+    // if(df){
+    //       cout<<"Don't take anything"<<endl;
+    // }
+
+
+
+// largest of 3 numbers
+     int x,y,z;
+    cin>>x>>y>>z;//
+
+
+    if(x>y and x>z){
+        cout<<"x is greater"<<endl;
+    }
+    else if(y>z and y>x){
+        cout<<"y is greater"<<endl;
+
+    }
+    else{
+        cout<<"z is greater"<<endl;
+
+    }
+    
+    
+
+
+
+
+
+
+
+
+
+
+    
+
+    return 0;
+   
+}
